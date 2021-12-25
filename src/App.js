9b4d1b5222cf49a552427d49add71e607179b438
Wendy-bear <w6363980@gmail.com>
2021-12-25 20:17:31 +0300

    +

--- src/App.js
+++ src/App.js
@@ -9,30 +9,21 @@ function App() {
         price="300"
         speed="До 10 Мбит/сек."
         description="Объем включенного трафика не ограничен"
-        isRate300={true}
-        isRate450={false}
-        isRate550={false}
-        isRate1000={false}
+        
       ></Rate>
       <Rate
         title="Безлимитный 450"
         price="450"
         speed="До 50 Мбит/сек."
         description="Объем включенного трафика не ограничен"
-        isRate300={false}
-        isRate450={true}
-        isRate550={false}
-        isRate1000={false}
+        
       ></Rate>
       <Rate
         title="Безлимитный 550"
         price="550"
         speed="До 100 Мбит/сек."
         description="Объем включенного трафика не ограничен"
-        isRate300={false}
-        isRate450={false}
-        isRate550={true}
-        isRate1000={false}
+        
         isSelected={true}
       ></Rate>
       <Rate
@@ -40,10 +31,8 @@ function App() {
         price="1000"
         speed="До 200 Мбит/сек."
         description="Объем включенного трафика не ограничен"
-        isRate300={false}
-        isRate450={false}
-        isRate550={false}
-        isRate1000={true}
+        
+        
       ></Rate>
     </>
   );
--- src/components/tarif.jsx
+++ src/components/tarif.jsx
@@ -1,64 +1,27 @@
 import "./tarif.css";
+import React, { useState } from "react";
 
-function Rate({
-  title,
-  price,
-  speed,
-  description,
-  isRate300,
-  isRate450,
-  isRate550,
-  isRate1000,
-  isSelected,
-}) {
+function Rate({ title, price, speed, description }) {
+  let titleClassname = "title-bg" + price;
+  let priceClassname = "price-bg" + price;
+
+  const [select, setSelect] = useState(false);
+
+  const handleSelect = () => {
+    setSelect(!select);
+  };
+
   return (
     <div className="wraper">
-      <div className={"card-wraper " + (isSelected ? "selected" : "")}>
-        <div
-          className={
-            "card-title " +
-            (isRate300 ? "title-bg300" : "") +
-            (isRate450 ? "title-bg450" : "") +
-            (isRate550 ? "title-bg550" : "") +
-            (isRate1000 ? "title-bg1000" : "")
-          }
-        >
-          {title}
-        </div>
-        <div className="card-price">
-          <div
-            className={
-              "card-price-currency " +
-              (isRate300 ? "price-bg300" : "") +
-              (isRate450 ? "price-bg450" : "") +
-              (isRate550 ? "price-bg550" : "") +
-              (isRate1000 ? "price-bg1000" : "")
-            }
-          >
-            руб.
-          </div>
-          <div
-            className={
-              "card-price-rate " +
-              (isRate300 ? "price-bg300" : "") +
-              (isRate450 ? "price-bg450" : "") +
-              (isRate550 ? "price-bg550" : "") +
-              (isRate1000 ? "price-bg1000" : "")
-            }
-          >
-            {price}
-          </div>
-          <div
-            className={
-              "card-price-term " +
-              (isRate300 ? "price-bg300" : "") +
-              (isRate450 ? "price-bg450" : "") +
-              (isRate550 ? "price-bg550" : "") +
-              (isRate1000 ? "price-bg1000" : "")
-            }
-          >
-            /мес.
-          </div>
+      <div
+        onClick={handleSelect}
+        className={"card-wraper " + (select ? "selected" : "")}
+      >
+        <div className={"card-title " + (titleClassname || "")}>{title}</div>
+        <div className={"card-price " + (priceClassname || "")}>
+          <div className="card-price-currency">руб.</div>
+          <div className="card-price-rate">{price}</div>
+          <div className="card-price-term">/мес.</div>
         </div>
         <div className="card-speed">{speed}</div>
         <div className="card-description">{description}</div>
