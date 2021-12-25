313974b017e3fb8b3d3758c894a81a40bf6d7275
Wendy-bear <w6363980@gmail.com>
2021-12-25 20:57:00 +0300

    +

--- src/App.js
+++ src/App.js
@@ -9,30 +9,24 @@ function App() {
         price="300"
         speed="До 10 Мбит/сек."
         description="Объем включенного трафика не ограничен"
-        
       ></Rate>
       <Rate
         title="Безлимитный 450"
         price="450"
         speed="До 50 Мбит/сек."
         description="Объем включенного трафика не ограничен"
-        
       ></Rate>
       <Rate
         title="Безлимитный 550"
         price="550"
         speed="До 100 Мбит/сек."
         description="Объем включенного трафика не ограничен"
-        
-        isSelected={true}
       ></Rate>
       <Rate
         title="Безлимитный 1000"
         price="1000"
         speed="До 200 Мбит/сек."
         description="Объем включенного трафика не ограничен"
-        
-        
       ></Rate>
     </>
   );
